6dd01e961446aeff3487821f46a3d34ee817d70a
BlurryfaceP1 <prathmeshwavhal@gmail.com>
2021-02-07 16:52:53 +0530

    Assignment 1 submitted

---
+++ Assignment 1 LetsUp DSA/Assignment 1 Q1.py
@@ -0,0 +1,17 @@
+
+a=int(input("Enter first number:"))
+b=int(input("Enter Second number:"))
+
+add=int(a+b)
+sub=int(a-b)
+mul=int(a*b)
+div=int(a/b)
+
+print("Addition is:",add)
+print("subtraction is:",sub)
+print("Multiplication is:",mul)
+print("Division is:",div)
+
+
+
+    
---
+++ Assignment 1 LetsUp DSA/Assignment 1 Q2.py
@@ -0,0 +1,6 @@
+
+X=int(input("Enter first number:"))
+Y=int(input("Enter second number:"))
+
+
+print("Answer is:",pow(X,Y))
